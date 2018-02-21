a3e5bfd12303792bc01301024f699aa3278119b8
lucl310 <lucthehuman21@gmail.com>
2018-02-21 08:57:47 -1000

    added basic functionality

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var pushes = 0
+if (pushes >= 1) {
+	document.getElementById('actualWords') = "Sorry Kids, Magic Isnt Real. Neither Is Santa."
+	var pushes = pushes - 1
+}
+if (pushes === 0) {
+	document.getElementById('actualWords') = ""
+}
